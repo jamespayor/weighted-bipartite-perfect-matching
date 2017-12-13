963dbf7867e24b542436fa9959c2abf83e4c2d21
James Payor <jamespayor@users.noreply.github.com>
2017-12-14 07:56:19 +1100

    Update hungarian.h

--- hungarian.h
+++ hungarian.h
@@ -2,7 +2,6 @@
 #define HUNGARIAN_H
 
 #include <vector>
-#include <utility>
 
 struct WeightedBipartiteEdge {
     const int left;
@@ -10,7 +9,7 @@ struct WeightedBipartiteEdge {
     const int cost;
 
     WeightedBipartiteEdge() : left(), right(), cost() {}
-    WeightedBipartiteEdge(int left, int right, int cost) : left(left), right(right), cost(cost) {}
+    WeightedBipartiteEdge(const int left, const int right, const int cost) : left(left), right(right), cost(cost) {}
 };
 
 const std::vector<int> hungarianMinimumWeightPerfectMatching(int n, const std::vector<WeightedBipartiteEdge> allEdges);
