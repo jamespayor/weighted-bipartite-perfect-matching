faed2d65cc28aa89f80907ee2cb608fc098de5ee
James Payor <jamespayor@users.noreply.github.com>
2017-12-14 08:38:50 +1100

    Add documentation.

--- hungarian.h
+++ hungarian.h
@@ -12,6 +12,10 @@ struct WeightedBipartiteEdge {
     WeightedBipartiteEdge(const int left, const int right, const int cost) : left(left), right(right), cost(cost) {}
 };
 
+// Given the number of nodes on each side of the bipartite graph and a list of edges, returns a minimum-weight perfect matching.
+// If a matching is found, returns a length-n vector, giving the nodes on the right that the left nodes are matched to.
+// If no matching exists, returns an empty vector.
+// (Note: Edges with endpoints out of the range [0, n) are ignored.)
 const std::vector<int> hungarianMinimumWeightPerfectMatching(int n, const std::vector<WeightedBipartiteEdge> allEdges);
 
 
