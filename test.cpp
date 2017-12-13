350961f942e25cd0637d30da03edddac918ef37c
James Payor <james@payor.com.au>
2017-12-13 20:41:34 +1100

    Add a running-time test.

--- test.cpp
+++ test.cpp
@@ -43,34 +43,63 @@ const std::vector<int> bruteForce(const int n, const std::vector<WeightedBiparti
 }
 
 int main() {
-	int n = 10;
-	std::vector<WeightedBipartiteEdge> edges;
-	for (int i = 0; i < n; ++i) {
-		for (int j = i - 2; j < i + 2; ++j) {
-			edges.push_back(WeightedBipartiteEdge(i, (j + n) % n, rand() % 100));
-		}
-	}
+	std::cout << "Running test on small random graph with ground truth (by brute force)..." << std::endl;
 
-	std::vector<int> groundTruthMatching = bruteForce(n, edges);
-	for (int i = 0; i < n; ++i) {
-		std::cout << i << " -> " << groundTruthMatching[i] << '\n';
-	}
-	std::cout << std::endl;
+    {
+        int n = 10;
+        std::vector<WeightedBipartiteEdge> edges;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i - 2; j < i + 2; ++j) {
+                edges.push_back(WeightedBipartiteEdge(i, (j + n) % n, rand() % 7));
+            }
+        }
 
-	std::vector<int> matching = hungarianMinimumWeightPerfectMatching(n, edges);
-	if (matching == groundTruthMatching) {
-		std::cout << "(Hungarian algorithm output matches brute force.)" << std::endl;
-	} else {
-		std::cout << std::endl << "Hungarian algorithm output doesn't agree with brute force:\n";
-		if (matching.empty()) {
-			std::cout << "=> No matching was found.\n";
-		} else {
-			for (int i = 0; i < n; ++i) {
-				std::cout << i << " -> " << matching[i] << '\n';
-			}
-		}
-		std::cout << std::endl;
-	}
+        std::vector<int> groundTruthMatching = bruteForce(n, edges);
+
+        std::cout << "\nGround truth matching:\n";
+        for (int i = 0; i < n; ++i) {
+            std::cout << i << " -> " << groundTruthMatching[i] << '\n';
+        }
+        std::cout << std::endl;
+
+        std::vector<int> matching = hungarianMinimumWeightPerfectMatching(n, edges);
+        if (matching == groundTruthMatching) {
+            std::cout << "Hungarian algorithm output matches brute force." << std::endl;
+        } else {
+            std::cout << std::endl << "Hungarian algorithm output does *not* agree with brute force:\n";
+            if (matching.empty()) {
+                std::cout << "=> No matching was found.\n";
+            } else {
+                for (int i = 0; i < n; ++i) {
+                    std::cout << i << " -> " << matching[i] << '\n';
+                }
+            }
+            std::cout << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+
+    std::cout << "\nRunning test on large random graph (matching should exist, algorithm shouldn't crash)..." << std::endl;
+
+    {
+        int n = 100000;
+        int m = 10000000;
+        std::vector<WeightedBipartiteEdge> edges;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i - (m / n / 2); j < i + (m / n / 2); ++j) {
+                edges.push_back(WeightedBipartiteEdge(i, (j + n) % n, rand() % 3));
+            }
+        }
+
+        std::vector<int> matching = hungarianMinimumWeightPerfectMatching(n, edges);
+
+        std::cout << '\n';
+        if (matching.empty()) {
+            std::cout << "Failure: Hungarian algorithm didn't find a matching." << std::endl;
+        } else {
+            std::cout << "Matching was found." << std::endl;
+        }
+    }
 
 	return EXIT_SUCCESS;
 }
